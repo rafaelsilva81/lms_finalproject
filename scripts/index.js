108ea3c38ade9c8b4b614003466e57c5d6122c4f
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-10-25 11:31:34 -0300

    feat: finished pokedex

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,33 @@
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 let currentPage = 1;
-const itemsPerPage = 10;
+const itemsPerPage = 60;
 
-/**
- * Retrieves a list of Pokemon from the API based on the given page number and items per page.
- * @async
- * @param {number} page - The page number to retrieve.
- * @returns {Promise<Array<{name: string, id: number, types: Array<string>}>>} - A promise that resolves to an array of objects containing the name, id, and types of each Pokemon.
- */
+function createPokemonCard(name, types, id) {
+  const pokemonCard = document.createElement("div");
+  pokemonCard.className = "pokemonCard";
+
+  const pokemonImg = document.createElement("img");
+  pokemonImg.className = "pokemonImage";
+  pokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+
+  const pokemonName = document.createElement("h2");
+  pokemonName.className = "pokemonName truncate";
+  pokemonName.innerText = name;
+
+  const pokemonId = document.createElement("small");
+  pokemonId.className = "pokemonNumber";
+  pokemonId.innerText = `#${id}`;
+  pokemonName.appendChild(pokemonId);
+
+  const pokemonType = document.createElement("p");
+  pokemonType.className = "pokemonType";
+  pokemonType.innerText = types.map((type) => type.type.name).join(", ");
+
+  pokemonCard.append(pokemonImg, pokemonName, pokemonType);
+
+  return pokemonCard;
+}
+
 async function listPokemon(page) {
   try {
     const response = await axios.get(apiUrl, {
@@ -19,35 +39,45 @@ async function listPokemon(page) {
 
     const pokemons = response.data.results;
 
-    const data = [];
     pokemons.forEach(async (pokemon) => {
       const pokemonData = await axios.get(pokemon.url);
       const { name, types, id } = pokemonData.data;
 
-      data.push({
-        name,
-        id,
-        types: types.map((type) => type.type.name),
-      });
+      const pokemonContainer = document.getElementById("pokemon-container");
+
+      const pokemonCard = createPokemonCard(name, types, id);
+      pokemonContainer.append(pokemonCard);
     });
   } catch (error) {
     console.error("Ocorreu um erro ao buscar os Pokémon:", error);
   }
 }
 
-function nextPage() {
-  currentPage++;
-  listPokemon(currentPage);
-}
+window.onload = async () => {
+  await listPokemon(currentPage);
 
-function previousPage() {
-  if (currentPage > 1) {
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+
+  previousButton.onclick = async (e) => {
+    e.preventDefault();
+    if (currentPage === 1) return;
+
+    //clear pokemon container
+    const pokemonContainer = document.getElementById("pokemon-container");
+    pokemonContainer.innerHTML = "";
     currentPage--;
-    listPokemon(currentPage);
-  } else {
-    console.log("Você está na primeira página.");
-  }
-}
+    await listPokemon(currentPage);
+  };
 
-// Listar a primeira página de Pokémon
-listPokemon(currentPage);
+  nextButton.onclick = async (e) => {
+    e.preventDefault();
+
+    //clear pokemon container
+    const pokemonContainer = document.getElementById("pokemon-container");
+    pokemonContainer.innerHTML = "";
+
+    currentPage++;
+    await listPokemon(currentPage);
+  };
+};
